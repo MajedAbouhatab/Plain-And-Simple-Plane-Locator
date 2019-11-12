608d3f07c1e3de1a9221ca7d7b7259eaf65e26ad
Majed Abouhatab, P.E <45211996+MajedAbouhatab@users.noreply.github.com>
2019-11-12 16:11:21 -0600

    Add files via upload

---
+++ PlaneLocator.ps1
@@ -0,0 +1,5 @@
+﻿$AZPath="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Tools\azsphere.exe"
+$VSBinPath=$ENV:UserProfile+"\source\repos\PlainAndSimple\PlaneLocator\"
+& $AZPath "dev" "sl" "delete"
+& $AZPath "dev" "sl" "deploy" "-p"$VSBinPath"bin\ARM\Release\PlaneLocator.imagepackage"
+& $AZPath "dev" "restart"
